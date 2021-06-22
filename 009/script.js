607f226bbc67127f7600eabb7d5ab1a2825ca445
smfssilva <smfssilva@yahoo.com>
2021-06-22 00:30:02 -0300

    Project 010 Dad Joke html

--- 009/script.js
+++ 009/script.js
@@ -13,7 +13,9 @@ sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
 
+
     btn.innerText = sound
+    
 
     btn.addEventListener('click', () => {
 
